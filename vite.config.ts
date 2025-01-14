2515a20bf6e5e35d02c2d9cb3e2624e8c3e18a4f
Ricardo Sacramento <sacramento@webeo.com>
2025-01-14 09:07:09 +0000

    feat: switch to solidjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,12 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        inlineDynamicImports: true,
-        entryFileNames: 'assets/js/main.js',
-        assetFileNames: 'assets/[ext]/main.[ext]',
-      }
-    }
+  plugins: [solidPlugin()],
+  server: {
+    port: 3000,
   },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+  build: {
+    target: 'esnext',
   },
 });
