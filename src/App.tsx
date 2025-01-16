418c9539a7be7e16fd83cbc922ca9e3099cf4d6e
Ricardo Sacramento <sacramento@webeo.com>
2025-01-16 18:52:43 +0000

    adding pie chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
 import styles from './App.module.css';
 import Header from './components/header/Header';
 import Card from './components/card/Card';
 import PieChart from './components/pieChart/PieChart';
+import 'normalize.css';
 
 const App: Component = () => {
 
   return (
     <div class={styles.App}>
       <Header />
-      <Card />
-      <PieChart />
+      <div class={styles.box}>
+        <Card />
+        <PieChart />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 };
--- src/components/pieChart/PieChart.tsx
+++ src/components/pieChart/PieChart.tsx
@@ -1,15 +1,17 @@
-import { DefaultChart } from 'solid-chartjs'
+import { onMount } from 'solid-js'
+import { Chart, Title, Tooltip, Legend, Colors } from 'chart.js'
+import { Pie } from 'solid-chartjs'
 import type { Component } from 'solid-js';
+import './PieChart.css';
 
 
 const PieChart: Component = () => {
 
+  onMount(() => {
+    Chart.register(Title, Tooltip, Legend, Colors)
+  })
+
   const chartData = {
-    labels: [
-      'Red',
-      'Blue',
-      'Yellow'
-    ],
     datasets: [{
       label: 'My First Dataset',
       data: [300, 50, 100],
@@ -18,16 +20,24 @@ const PieChart: Component = () => {
         'rgb(54, 162, 235)',
         'rgb(255, 205, 86)'
       ],
-      hoverOffset: 10,
-      borderColor: 'rgb(255, 99, 132)',
-      borderDash: [20, 5]
+      hoverOffset: 4,
     }]
   }
 
+  const chartOptions = {
+    responsive: false,
+  }
+
   return (
-    <div>
-      <DefaultChart type="pie" data={chartData} width={500} height={500} />
-    </div>)
+    <div class="pie-chart">
+      <div class="labels">
+        <span>Red</span>
+        <span>Blue</span>
+        <span>Yellow</span>
+      </div>
+      <Pie data={chartData} options={chartOptions} width={250} height={250} />
+    </div>
+  )
 }
 
 export default PieChart;
