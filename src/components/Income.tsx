f46faea2a343e17714a7561cf200a74f5c62906b
Ricardo Sacramento <ricardo.sacramento18@gmail.com>
2025-01-26 08:47:10 +0000

    adding income css styles

--- src/components/Income.tsx
+++ src/components/Income.tsx
@@ -8,11 +8,15 @@ export type IncomeProps = {
 
 const Income: Component<IncomeProps> = (props) => {
   return (
-    <div>
-      <h3>{props.title}</h3>
-      <p>Before Tax: {props.beforeTax}</p>
-      <p>After Tax: {props.afterTax}</p>
-    </div>
+    <div class="p-5 mr-10 my-5 rounded border-2 min-w-[200px]">
+      <div class="flex justify-between items-center mb-5">
+        <div class="p-4 rounded-full bg-sectionBg text-white text-3xl w-[65px] h-[65px]">
+          <i class="fa-solid fa-hand-holding-dollar"></i>
+        </div>
+        <h2>{props.title}</h2>
+      </div>
+      <p class="mx-2 text-2xl">+{props.afterTax}€</p>
+    </div >
   );
 }
 
--- src/components/IncomeSection.tsx
+++ src/components/IncomeSection.tsx
@@ -23,9 +23,9 @@ const IncomeSection: Component = () => {
     }
   ]
   return (
-    <div class="my-5 p-5 rounded shadow-card w-full">
+    <div class="m-5 p-5 rounded shadow-card w-full">
       <h2>Income</h2>
-      <div class="flex items-align justify-evenly">
+      <div class="flex items-align justify-start">
         <For each={incomes}>
           {(income) => (
             <Income
